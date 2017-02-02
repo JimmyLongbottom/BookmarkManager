8319721448c60abaf3fe37b7720fb479e9a2e0d1
Jim Swanson <timsawyerpdx@gmail.com>
2017-02-01 20:46:25 -0800

    create subject component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Subject from ./Subject.js;
 class App extends Component {
   constructor (){
     super();
 
-this.state = {
-  resources: [{title: "Thinking in React",
-                url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
-              {title: "another Title",
-                url: "https://www.youtube.com/watch?v=vu_rIMPROoQ"},
-              {title: "again, another title",
-                url:"https://www.youtube.com/watch?v=IEf1KAcK6A8"}
-                {title: "Review higher order functions",
-                 url: https//www.youtube.com/watch?v=BMUiFMZr7vk&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84}
-               ]
-};
+this.state = {resources: [
+   {subject: "Functional Programming Basics",
+    resources: [{
+     title: "Thinking in React",
+     url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
+
+     {title: "Given Title",
+      url: "#"}]
+
+  {subject: "given subject"
+    resources: [
+      {
+      title: "another Title",
+      url: "https://www.youtube.com/watch?v=vu_rIMPROoQ"
+    },
+      title: "another given title",
+      url: "#"
+    }
+
+
+  {subject: ES6,
+          resources : [{
+            title: "again, another title",
+            url:"https://www.youtube.com/watch?v=IEf1KAcK6A8"}]
+    }
+
+}};
 
 }
 //eveent handlers her
   render() {
     return(
     <div>
-      {this.state.resources.map((resource) =>{
-        return <a href = {resource.url}>{resource.title}</a>
-      })}
+      Parent Component
+      <Subject />
     </div>
     );
   }
---
+++ src/Subject.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class Subject extends Component {
+    constructor(props) {
+      super(props)
+    }
+
+
+    //event handlers here
+
+    render() {
+      return(
+        <div>
+        
+        </div>
+      )
+    }
+}
