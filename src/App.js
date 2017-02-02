07f78957c1fbe6d64c5165266d2d38a174e6f948
Jim Swanson <timsawyerpdx@gmail.com>
2017-02-01 19:26:47 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor (){
+    super();
+  }
+this.state = {};
+
+}
+
+//eveent handlers her
+  render() {
+    return(
+      <a href='https://facebook.github.io/react/docs/thinking-in-react.html'></a>
+    );
+  }
+}
+
+
+export default App;
