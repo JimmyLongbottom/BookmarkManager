0707a03babf055cb191481651019620d8eb0aa17
Jim Swanson <timsawyerpdx@gmail.com>
2017-02-08 12:25:34 -0800

    put andrew's code in tohelp find my bug.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,140 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Subject from "./Subject.js";
+//
+// class App extends Component {
+//   constructor (){
+//     super();
+//
+// this.state = {
+//   resources: [
+//     {
+//       subject: "Functional Programming Basics: Week 1",
+//         resources: [
+//           {
+//             title: "Thinking in React",
+//             url: "https://facebook.github.io/react/docs/thinking-in-react.html"
+//           },
+//
+//           {
+//             title: "Thinking in React",
+//             url: "https://facebook.github.io/react/docs/thinking-in-react.html"
+//           }]
+//
+//       },
+//
+//     {
+//       subject: " Programming Basics: Week 2",
+//         resources: [
+//         {
+//           title: "Thinking in React",
+//           url: "https://facebook.github.io/react/docs/thinking-in-react.html"
+//         },
+//
+//         {
+//           title: "yep",
+//           url: "#"
+//         }
+//       ]
+//     }
+//
+//
+//     ]
+//
+// }
+//
+// //beg render func
+//                             //event handlers here
+//   render (){
+//     return (
+//       <div>
+//         {this.state.resources.map}((resource) =>{
+//           return(
+//             <li>
+//             <Subject items = {resource}/>
+//           </li>
+//           )
+//         })
+//
+//       </div>
+//     );
+//   }
+//
+// }
+//
+//
+// export default App;
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Subject from ./Subject.js;
+import Subject from './Subject.js';
+
 class App extends Component {
-  constructor (){
+  constructor() {
     super();
 
-this.state = {resources: [
-   {subject: "Functional Programming Basics",
-    resources: [{
-     title: "Thinking in React",
-     url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
+    this.state = {resources: [
 
-     {title: "Given Title",
-      url: "#"}]
-
-  {subject: "given subject"
-    resources: [
       {
-      title: "another Title",
-      url: "https://www.youtube.com/watch?v=vu_rIMPROoQ"
-    },
-      title: "another given title",
-      url: "#"
-    }
+        subject: "Functional Programming Basics",
+        resources: [
+          {
+            title: "FunFunFunction Functional Programming Playlist",
+            url: "https://www.youtube.com/watch?v=BMUiFMZr7vk&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84"
+          },
+          {
+            title: "FunFunFunction var, let, and const",
+            url: "https://www.youtube.com/watch?v=sjyJBL5fkp8"
+          },
+          {
+            title: "FunFunFunction arrow functions",
+            url: "https://www.youtube.com/watch?v=6sQDTgOqh-I"
+          }
+        ]
+      },
 
 
-  {subject: ES6,
-          resources : [{
-            title: "again, another title",
-            url:"https://www.youtube.com/watch?v=IEf1KAcK6A8"}]
-    }
+      {
+        subject: "ES6",
+        resources: [
+          {
+            title: "Kyle Robinson Young ES6 Essentials",
+            url: "https://www.youtube.com/watch?v=CozSF5abcTA"
+          }
+        ]
+      },
 
-}};
 
-}
-//eveent handlers her
+      {
+        subject: "Intro to React",
+        resources: [
+          {
+            title: "Thinking in React",
+            url: "https://facebook.github.io/react/docs/thinking-in-react.html"},
+          {
+            title: "Mindspace React Tutorial",
+            url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"},
+          {
+            title: "LearnCode Academy React Tutorial",
+            url: "https://youtu.be/fd2Cayhez58"
+          }
+        ]
+      }
+
+    ]}
+  }
+
   render() {
-    return(
-    <div>
-      Parent Component
-      <Subject />
-    </div>
+
+    return (
+      <div style={{color:'darkBlue'}}>
+        Parent Component
+        <Subject items={this.state.resources[0]}/>
+        <Subject items={this.state.resources[1]}/>
+        <Subject items={this.state.resources[2]}/>
+      </div>
     );
   }
 }
-
 
 export default App;
--- src/Subject.js
+++ src/Subject.js
@@ -1,18 +1,62 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+//
+// export default class Subject extends Component {
+//     constructor(props) {
+//       super(props)
+//     }
+//
+//
+//     //event handlers here
+//
+//     render() {
+//       return(
+//         <div>
+//           <h2>{this.props.items.subject}</h2>
+//
+//           <ul>
+//             {this.props.items.resources.map((resource) => {
+//               return(
+//               <li>
+//                 <a href ={resource.url}>{resource.title}</a>
+//               </li>
+//             )}
+//
+//           )}
+//
+//           </ul>
+//       </div>
+//     )
+// }
+//
+// }
+//
+//
+import React, { Component } from 'react';
 
 export default class Subject extends Component {
-    constructor(props) {
-      super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
+  // event handlers
 
-    //event handlers here
+  render() {
+    return(
+      <div >
+        <h2 style={{color:'brown'}}>{this.props.items.subject}</h2>
 
-    render() {
-      return(
-        <div>
-        
-        </div>
-      )
-    }
+        <ul>
+          {this.props.items.resources.map((resource) => {
+            return(
+              <li>
+                <a style={{color:'green'}} href={resource.url}>{resource.title}</a>
+              </li>
+            )}
+
+          )}
+        </ul>
+      </div>
+    )
+  }
+
 }
