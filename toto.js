97a5881da253fe2f072301e84b126cf41c6337f1
Jim Swanson <timsawyerpdx@gmail.com>
2017-02-08 18:22:41 -0800

    tiny little changes

---
+++ toto.js
@@ -0,0 +1,9 @@
+BookMark Managar for React:
+
+*store  resources as links hardcoded in component render
+*store resources in state
+*break state object resources in..
+*create subject Componentrender subject title per Component
+*Modify app.js + subject.js props to accomodate new state structure
+*Display list if resource titles and links per componentld components in App;js
+*programatically generate children
